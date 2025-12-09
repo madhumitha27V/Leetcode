ab1f38acf5834202d668b26f674057d127e015df
Madhumitha V <madhumithav.23eee@kongu.edu>
2025-12-09 06:30:20 +0530

    Time: 0 ms (100%), Space: 17.1 MB (90.71%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.c
+++ 0344-reverse-string/0344-reverse-string.c
@@ -1,13 +1,13 @@
 void reverseString(char* s, int sSize) {
-    char temp[sSize];
+    int result[sSize];
     int k = 0;
-
-    for (int i = sSize - 1; i >= 0; i--) {
-        temp[k] = s[i];
+    for(int i = sSize - 1; i >= 0 ; i--){
+        result[k] = s[i];
         k++;
     }
-
-    for (int i = 0; i < sSize; i++) {
-        s[i] = temp[i];
+    for(int i = 0 ; i < sSize ; i++){
+        s[i] = result[i];
     }
 }
+
+
