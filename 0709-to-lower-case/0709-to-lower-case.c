cdc0569f0131484203ac62c12904ad392f575cfb
Madhumitha V <madhumithav.23eee@kongu.edu>
2025-12-11 06:43:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.89%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.c
@@ -0,0 +1,12 @@
+char* toLowerCase(char* s) {
+    for(int i = 0 ; i < strlen(s) ; i++){
+        if (s[i] >= 65 && s[i] <= 90){
+            s[i] = s[i] + 32;
+        }
+        else{
+            s[i] = s[i];
+        }
+        
+    }
+    return s;
+}
