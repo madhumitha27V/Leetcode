e3a89bd0972ff17ad7f261d6aedd8de0dffe6fe9
Madhumitha V <madhumithav.23eee@kongu.edu>
2025-12-15 20:22:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.3%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.c
+++ 0771-jewels-and-stones/0771-jewels-and-stones.c
@@ -1,11 +1,11 @@
 int numJewelsInStones(char* jewels, char* stones) {
     int count = 0;
-    for(int i = 0 ; i < strlen(jewels) ; i++){
-        for(int j = 0 ; j < strlen(stones) ; j++){
-        if(jewels[i] == stones[j]){
-            count = count + 1;
+    for(int i = 0 ; i < jewels[i] ; i++){
+        for(int j = 0 ; j < stones[j] ; j++){
+            if(jewels[i] == stones[j]){
+                count = count+1;
+            }
         }
-    }
     }
     return count;
 }
