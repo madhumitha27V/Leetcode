037235759715c1ab55da79f1fe83c6cab54b8703
Madhumitha V <madhumithav.23eee@kongu.edu>
2025-12-25 09:54:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.c
@@ -0,0 +1,12 @@
+int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+    int sum = arrivalTime + delayedTime;
+    if(sum <= 23){
+        return sum;
+    }
+    else if(sum == 24){
+        return 0;
+    }
+    else{
+        return sum-delayedTime;
+    }
+}
