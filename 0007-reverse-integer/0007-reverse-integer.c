092c37b7094b01c350b1e0f08f7fce4a84fc3dcd
Madhumitha V <madhumithav.23eee@kongu.edu>
2025-12-27 06:44:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.5%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -1,14 +1,16 @@
-int reverse(int x) {
-    int rev = 0;
-    int a;
-    while (x != 0) {
+#include<limits.h>
+int reverse(int x){
+    int a=0;
+    long rev = 0;
+    while(x != 0){
         a = x % 10;
         rev = rev * 10 + a;
         x = x / 10;
+
     }
+
     if(rev < INT_MIN || rev > INT_MAX){
         return 0;
     }
-    return rev;
-
+   return rev;
 }
