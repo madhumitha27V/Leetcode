31c21b8f8ea74919c639e86d61ca34e82d9acf94
Madhumitha V <madhumithav.23eee@kongu.edu>
2026-05-03 13:02:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (77.55%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.c
+++ 0796-rotate-string/0796-rotate-string.c
@@ -1,8 +1,12 @@
 #include <string.h>
 #include <stdbool.h>
+
 bool rotateString(char* s, char* goal) {
-  if(strcmp(s,goal) == 0){
-    return true;
-  }
-  return false;
+    if (strlen(s) != strlen(goal)) return false;
+
+    char temp[201];  
+    strcpy(temp, s);
+    strcat(temp, s);
+
+    return strstr(temp, goal) != NULL;
 }
